91ec996a41e60cc45e3bd5255b619de891277589
Valeriects <valerie.cts13@gmail.com>
2022-01-31 07:51:51 +0100

    tp-recette

---
+++ tp-recette-aleatoire/readme.md
@@ -0,0 +1,10 @@
+# **Recette aléatoire**
+
+* Mini TP : Mise en application
+
+
+*   Les datas, les directives, le binding, et le FETCH !
+  *   Créer une page qui affiche les détails d'une recette prise au hasard sur TheMealDB API.
+
+*   Pour cela, nous utiliserons le fetch() sur l'URL suivante : https://www.themealdb.com/api/json/v1/1/random.php
+  *   qui nous renvoit automatiquement une recette au hasard.
--- tp-recette-aleatoire/tp_recette_aleatoire.js
+++ tp-recette-aleatoire/tp_recette_aleatoire.js
@@ -1,14 +1,3 @@
-// Mini TP : Mise en application
-
-
-//   Les datas, les directives, le binding, et le FETCH !
-//   Créer une page qui affiche les détails d'une recette prise au hasard sur TheMealDB API.
-
-//   Pour cela, nous utiliserons le fetch() sur l'URL suivante : https://www.themealdb.com/api/json/v1/1/random.php
-//   qui nous renvoit automatiquement une recette au hasard.
-// console.log("coucou")
-
-// par convention on délcare les applications pour ke l'utilisateur n epuisse le changer dans le code
 const recette = Vue.createApp({
     
     data() {
